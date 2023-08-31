07dc0873a7b22f08abdd0f8e05604de18ced2d84
mruzairnaseer <uzairnaseer51@gmail.com>
2023-08-31 15:26:23 +0500

    conditionals done

--- Script.js
+++ Script.js
@@ -176,4 +176,22 @@ console.log(!true)
 // codewithharry 
 */
 
+let a = prompt("Hey whats you age?"); // input in the browser with the prompt
+a = Number.parseInt(a); // Converting the string to a number
+if(a<0){
+  alert("This is an invalid age");
+}
+else if(a<9){
+  alert("You are a kid and you cannot even think of driving");
+}
+else if(a<18 && a>=9){
+  alert("You are a kid and you can think of driving after 18");
+}
+else{
+  alert("You can now drive as you are above 18");
+}
+console.log("Done")
+// HomeWork - Explore switch statement and write a basic program in the comments
+console.log("You can", (a<18? "not drive" :"drive")) //ternary operator used here
+
 //console.log(start here!)
